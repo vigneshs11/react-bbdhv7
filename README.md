82e78d242455887872d3e182418c122b2b1a9bfb
vigneshs11 <vigneshswaminathan11@gmail.com>
2022-03-05 13:25:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bbdhv7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bbdhv7)
---
+++ src/components/toast/Toast.tsx
@@ -0,0 +1,29 @@
+ 
+import React from 'react'
+
+
+
+class ToastWrapper extends React.Component {
+
+
+   const TYPE = [
+
+    {type: 'success',
+      color:  '#22c35e'},
+      {type: 'error',
+      color:  '#E53E3E'}
+   ]
+
+   const DURATION = 300
+   
+
+
+
+ 
+
+   render() {
+    return <></>
+   }
+
+
+}
